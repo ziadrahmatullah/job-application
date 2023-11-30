62a587642c5bc73b6c751600b5f13e3c88875c0d
Muhammad Ziad Rahmatullah <ziad.rahmatullah@shopee.com>
2023-11-30 10:36:58 +0700

    feat: add dml

---
+++ sql/dml.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (name, current_job, age, created_at, updated_at)
+VALUES
+('Alice', 'Job Researcher', 18, NOW(), NOW()),
+('Bob', 'Job Researcher', 18, NOW(), NOW());
+
+INSERT INTO jobs(name, company, quota, created_at, updated_at)
+VALUES
+('Backend Developer', 'Shopee', 2, NOW(), NOW()),
+('Frontend Developer', 'Shopee', 2, NOW(), NOW());
