405b2213fa80a7162a69911f42f531e5f60f9bbb
Muhammad Ziad Rahmatullah <ziad.rahmatullah@shopee.com>
2023-11-30 10:50:41 +0700

    feat: add connect database

---
+++ database/connect.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+
+	dsn := fmt.Sprintf(`
+		host=%s
+		user=%s
+		password=%s
+		dbname=%s
+		port=%s
+		sslmode=disable`, dbHost, dbUser, dbPass, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Can't connect to database: ", err)
+	}
+	return db
+}
